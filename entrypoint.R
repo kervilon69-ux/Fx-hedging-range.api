2464a6af5b18ff9f87bbdb14f4472820336f4641
kervilon69-ux <kervilon69@gmail.com>
2026-06-04 09:52:11 +0200

    Update entrypoint.R

--- entrypoint.R
+++ entrypoint.R
@@ -1,2 +1,2 @@
 library(plumber)
-pr("plumber.R") %>% pr_run(host = "0.0.0.0", port = as.integer(Sys.getenv("PORT", 8001)))
+pr("api_range.R") %>% pr_run(host = "0.0.0.0", port = as.integer(Sys.getenv("PORT", 8001)))
